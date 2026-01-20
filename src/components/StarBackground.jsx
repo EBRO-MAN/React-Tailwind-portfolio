0f5e153ed00b0c72a815a72e6ef661dbb381dcae
Ebro <ibrahimabdulwehab@gmail.com>
2026-01-20 10:13:37 -0800

    first commit

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+
+// id, size, x, y, opacity, animationDuration
+// id, size, x, y, delay, animationDuration
+
+
+export const StarBackground = () => {
+    const [stars, setStars] = useState([]);
+    const [meteors, setMeteors] = useState([]);
+
+    useEffect(() => {
+        generateStars();
+        generateMeteors();  
+
+        const handleResize = () => {
+            generateStars();
+        };
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const generateStars = () => {
+        const numberOfStars = Math.floor(
+        (window.innerWidth * window.innerHeight) / 10000
+        );
+
+
+        const newStars = [];
+
+        for (let i = 0; i < numberOfStars; i++) {
+            newStars.push({
+                id: i,
+                size: Math.random() * 3 + 1, // Size between 1 and 3
+                x: Math.random() * 100, // Percentage
+                y: Math.random() * 100, // Percentage
+                opacity: Math.random() * 0.5 + 0.5, // Opacity between 0.1 and 0.6
+                animationDuration: Math.random() * 4 + 2, // Duration between 5s and 10s
+            });
+        }
+        setStars(newStars);
+    };
+        
+    
+    const generateMeteors = () => {
+        const numberOfMeteors = 4;
+
+
+        const newMeteors = [];
+
+        for (let i = 0; i < numberOfMeteors; i++) {
+            newMeteors.push({
+                id: i,
+                size: Math.random() * 2 + 1, // Size between 1 and 3
+                x: Math.random() * 100, // Percentage
+                y: Math.random() * 20, // Percentage
+                delay: Math.random() * 50, // Delay between 0s and 50s
+                animationDuration: Math.random() * 3 + 3, // Duration between 5s and 10s
+            });
+        }
+        setMeteors(newMeteors);
+    };
+
+    return (
+        <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+            {stars.map((star) => (
+                <div 
+                 key={star.id}
+                 className="star animate-pulse-subtle" 
+                 style={{
+                    width: star.size + "px",
+                    height: star.size + "px",
+                    left: star.x + "%",
+                    top: star.y + "%",
+                    opacity: star.opacity,
+                    animationDuration: star.animationDuration + "s", 
+                }}>
+
+                </div>
+            ))}
+            {meteors.map((meteor) => (
+                <div key={meteor.id} className="meteor animate-meteor" style={{
+                    width: meteor.size * 50 + "px",
+                    height: meteor.size * 2 + "px",
+                    left: meteor.x + "%",
+                    top: meteor.y + "%",
+                    animationDelay: meteor.delay ,
+                    animationDuration: meteor.animationDuration + "s", 
+                }}></div>
+            ))}
+            
+        </div>
+    );
+};
